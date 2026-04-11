7bdecf1eac5a8d5876634f66ca4e3a2b59b6ba42
Ted Vtorov <58144503+BUSH222@users.noreply.github.com>
2026-04-12 02:57:04 +0300

    cs8, cs16, cs32 support

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <core.h>
 #include <gui/style.h>
 #include <gui/widgets/file_select.h>
+#include <utils/optionlist.h>
 #include <filesystem>
 #include <regex>
 #include <gui/tuner.h>
@@ -16,6 +17,12 @@
 
 #define CONCAT(a, b) ((std::string(a) + b).c_str())
 #define MIN_SAMPLE_RATE 8000.0f
+
+enum SampleType {
+    SAMP_TYPE_CS8,
+    SAMP_TYPE_CS16,
+    SAMP_TYPE_CS32
+};
 
 SDRPP_MOD_INFO{
     /* Name:            */ "better_file_source",
@@ -29,8 +36,12 @@ ConfigManager config;
 
 class BetterFileSourceModule : public ModuleManager::Instance {
 public:
-    BetterFileSourceModule(std::string name) : fileSelect("", { "CS8 IQ Files (*.cs8)", "*.cs8", "CS8 IQ Files (*.c8)", "*.c8", "Raw Files (*.raw)", "*.raw", "All Files", "*" }) {
+    BetterFileSourceModule(std::string name) : fileSelect("", { "IQ Files", "*.cs8 *.c8 *.cs16 *.c16 *.cs32 *.c32 *.raw", "All Files", "*" }) {
         this->name = name;
+
+        sampleTypes.define(SAMP_TYPE_CS8, "CS8", SAMP_TYPE_CS8);
+        sampleTypes.define(SAMP_TYPE_CS16, "CS16", SAMP_TYPE_CS16);
+        sampleTypes.define(SAMP_TYPE_CS32, "CS32", SAMP_TYPE_CS32);
 
         if (core::args["server"].b()) { return; }
 
@@ -38,6 +49,12 @@ public:
         fileSelect.setPath(config.conf["path"], true);
         if (config.conf.contains("sampleRate")) {
             sampleRate = std::max((float)config.conf["sampleRate"], MIN_SAMPLE_RATE);
+        }
+        if (config.conf.contains("sampleType")) {
+            SampleType type = config.conf["sampleType"];
+            if (sampleTypes.keyExists(type)) {
+                sampleTypeId = sampleTypes.keyId(type);
+            }
         }
         config.release();
 
@@ -121,6 +138,15 @@ private:
 
         float menuWidth = ImGui::GetContentRegionAvail().x;
 
+        ImGui::BeginDisabled(_this->running);
+        ImGui::LeftLabel("Sample Type");
+        ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
+        if (ImGui::Combo(("##_sample_type_" + _this->name).c_str(), &_this->sampleTypeId, _this->sampleTypes.txt)) {
+            config.acquire();
+            config.conf["sampleType"] = _this->sampleTypes.key(_this->sampleTypeId);
+            config.release(true);
+        }
+
         ImGui::LeftLabel("Sample Rate");
         ImGui::SetNextItemWidth(menuWidth - ImGui::GetCursorPosX());
         if (ImGui::InputFloat(("##_sample_rate_" + _this->name).c_str(), &_this->sampleRate, 1.0f, 100.0f, "%.0f")) {
@@ -133,6 +159,7 @@ private:
                 core::setInputSampleRate(_this->sampleRate);
             }
         }
+        ImGui::EndDisabled();
 
         if (_this->fileSelect.render("##file_source_" + _this->name)) {
             if (_this->fileSelect.pathIsValid()) {
@@ -145,7 +172,6 @@ private:
                     if (!_this->reader->isValid()) {
                         throw std::runtime_error("Could not open file");
                     }
-                    // Enforce minimum sample rate of 8000 Hz
                     _this->sampleRate = std::max(_this->sampleRate, MIN_SAMPLE_RATE);
                     _this->reader->setSampleRate(_this->sampleRate);
                     core::setInputSampleRate(_this->sampleRate);
@@ -169,15 +195,35 @@ private:
         BetterFileSourceModule* _this = (BetterFileSourceModule*)ctx;
         double sampleRate = std::max(_this->sampleRate, MIN_SAMPLE_RATE);
         int blockSize = std::min((int)(sampleRate / 200.0f), (int)STREAM_BUFFER_SIZE);
-        int8_t* inBuf = new int8_t[blockSize * 2];
+        SampleType type = _this->sampleTypes.key(_this->sampleTypeId);
 
-        while (true) {
-            _this->reader->readSamples(inBuf, blockSize * 2 * sizeof(int8_t));
-            volk_8i_s32f_convert_32f((float*)_this->stream.writeBuf, inBuf, 128.0f, blockSize * 2);
-            if (!_this->stream.swap(blockSize)) { break; };
+        if (type == SAMP_TYPE_CS8) {
+            int8_t* inBuf = new int8_t[blockSize * 2];
+            while (true) {
+                _this->reader->readSamples(inBuf, blockSize * 2 * sizeof(int8_t));
+                volk_8i_s32f_convert_32f((float*)_this->stream.writeBuf, inBuf, 128.0f, blockSize * 2);
+                if (!_this->stream.swap(blockSize)) { break; };
+            }
+            delete[] inBuf;
         }
-
-        delete[] inBuf;
+        else if (type == SAMP_TYPE_CS16) {
+            int16_t* inBuf = new int16_t[blockSize * 2];
+            while (true) {
+                _this->reader->readSamples(inBuf, blockSize * 2 * sizeof(int16_t));
+                volk_16i_s32f_convert_32f((float*)_this->stream.writeBuf, inBuf, 32768.0f, blockSize * 2);
+                if (!_this->stream.swap(blockSize)) { break; };
+            }
+            delete[] inBuf;
+        }
+        else if (type == SAMP_TYPE_CS32) {
+            int32_t* inBuf = new int32_t[blockSize * 2];
+            while (true) {
+                _this->reader->readSamples(inBuf, blockSize * 2 * sizeof(int32_t));
+                volk_32i_s32f_convert_32f((float*)_this->stream.writeBuf, inBuf, 2147483648.0f, blockSize * 2);
+                if (!_this->stream.swap(blockSize)) { break; };
+            }
+            delete[] inBuf;
+        }
     }
 
     double getFrequency(std::string filename) {
@@ -200,12 +246,16 @@ private:
     std::thread workerThread;
 
     double centerFreq = 100000000;
+
+    OptionList<int, SampleType> sampleTypes;
+    int sampleTypeId = 0;
 };
 
 MOD_EXPORT void _INIT_() {
     json def = json({});
     def["path"] = "";
     def["sampleRate"] = 1000000.0f;
+    def["sampleType"] = SAMP_TYPE_CS8;
     config.setPath(core::args["root"].s() + "/better_file_source_config.json");
     config.load(def);
     config.enableAutoSave();
